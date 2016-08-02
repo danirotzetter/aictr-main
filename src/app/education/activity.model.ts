c6a9139bbd2c8296e2895d3c1e3fb558526777fa
Dani <danirotzetter@gmx.net>
2016-08-02 13:38:41 +0200

    Updating project capturing and models

--- src/app/education/activity.model.ts
+++ src/app/education/activity.model.ts
@@ -1,7 +1,9 @@
+import {Metric} from './metric.model';
 export class Activity{
     public _id: number;
     public date: Date;
     public name: String;
+    public metrics:Metric[];
     
     constructor(values: Object = {}) {
         Object.assign(this, values);
